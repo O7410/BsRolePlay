f1857f50c5138d6e09f1e9115b69eda53e523486
B4nduty <bandutysmods@gmail.com>
2024-01-07 20:54:34 +0100

    Initial commit

---
+++ src/main/java/banduty/bsroleplay/BsRolePlayDataGenerator.java
@@ -0,0 +1,11 @@
+package banduty.bsroleplay;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BsRolePlayDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
