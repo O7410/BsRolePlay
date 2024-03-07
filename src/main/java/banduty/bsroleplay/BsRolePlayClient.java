bb39d8e8e00402594122629e7327ffc4e63f2bfc
B4nduty <bandutysmods@gmail.com>
2024-03-07 18:34:40 +0100

    Version 1.4.2

--- src/main/java/banduty/bsroleplay/BsRolePlayClient.java
+++ src/main/java/banduty/bsroleplay/BsRolePlayClient.java
@@ -2,13 +2,13 @@ package banduty.bsroleplay;
 
 import banduty.bsroleplay.block.entity.ModBlockEntities;
 import banduty.bsroleplay.block.entity.client.HolyCloudStatueRenderer;
+import dev.architectury.registry.client.rendering.BlockEntityRendererRegistry;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 
 public class BsRolePlayClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        BlockEntityRendererFactories.register(ModBlockEntities.HOLY_CLOUD_STATUE_ENTITY, HolyCloudStatueRenderer::new);
+        BlockEntityRendererRegistry.register(ModBlockEntities.HOLY_CLOUD_STATUE_ENTITY, HolyCloudStatueRenderer::new);
     }
 }
